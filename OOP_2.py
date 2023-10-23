d6708a910e8538288f7733c5c8074a4ef32fee47
alosoz <alosbjk07@gmail.com>
2023-10-23 14:08:43 +0200

    23.10 14:08

---
+++ OOP_2.py
@@ -0,0 +1,11 @@
+# import OOP_1
+
+# account1 = OOP_1.BankAccount("12345", 100.0)
+
+# account1.user_menu
+
+from OOP_1 import BankAccount
+
+account1 = BankAccount("12345", 100.0)
+
+account1.user_menu
